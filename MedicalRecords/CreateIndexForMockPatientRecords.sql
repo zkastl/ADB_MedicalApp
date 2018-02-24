6e4bc0f1cf1ddda1cea2206a7fc2f24c8e36f5c6
Zachary Kastl <zkastl8@gmail.com>
2018-02-24 11:20:42 -0600

    add sql scripts for inserting lots of medical data, added vc# code for interacting with sql server, added new branch to test this

---
+++ MedicalRecords/CreateIndexForMockPatientRecords.sql
@@ -0,0 +1,4 @@
+﻿create clustered columnstore index idx on dbo.MOCK_PATIENT_RECORDS with (data_compression=columnstore_archive);
+GO
+create clustered index rowidx on dbo.MOCK_PATIENT_RECORDS (first_name, last_name);
+GO
---
+++ MedicalRecords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Basic idea
+            // 1. Create tables on database
+            // 2. create index row and column
+            // 3. Find some way to demonstrate this.
+            */
+        }
+    }
+}
